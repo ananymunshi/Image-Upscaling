799d30c00fd8e8fa70343a5d9497661dae27fb8e
Tanushree Pardhi <tcpardhi1999@gmail.com>
2021-02-17 17:12:50 +0530

    UI integration with interpolation + error calculation

--- UI/Page1.py
+++ UI/Page1.py
@@ -1,6 +1,8 @@
 # Imports
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+from functions import *
+from functools import partial
 
 
 # Root window
@@ -10,9 +12,17 @@ root.configure(bg="#333333")
 
 
 # Function to Upload Image
-def fileopener():
-    input = askopenfilename(initialdir="/",  filetypes=(("jpg files", "*.jpg"),("all files", "*.*")))
-    if not input:
+def fileopener_img():
+    global file_name1
+    file_name1 = askopenfilename(initialdir=r"D:\image_upscaling\Data\LR",  filetypes=(("png files", "*.png"),("all files", "*.*")))
+    if not file_name1:
+        return None
+
+# Function to upload reference image
+def fileopener_ref():
+    global file_name2
+    file_name2 = askopenfilename(initialdir=r"D:\image_upscaling\Data\HR",  filetypes=(("png files", "*.png"),("all files", "*.*")))
+    if not file_name2:
         return None
         
 # Button Image
@@ -25,22 +35,26 @@ ButtonImg6 = PhotoImage(file='Images/pixel.png')
 ButtonImg7 = PhotoImage(file='Images/ShowAll.png')
 
 # Image Upload function
-Button1 = Button(root, image=ButtonImg1, borderwidth=0, bg="#333333", command=fileopener)
-Button1.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+Button01 = Button(root, image=ButtonImg1, borderwidth=0, bg="#333333", command=fileopener_img)
+Button01.grid(row=0, column=0, padx=10, pady=10)
 
-Button2 = Button(root, image=ButtonImg2, bg="#333333", borderwidth=0)
+Button02 = Button(root, image=ButtonImg1, borderwidth=0, bg="#333333", command=fileopener_ref)
+Button02.grid(row=0, column=1, padx=10, pady=10)
+
+# bicubic interpolation method 
+Button2 = Button(root, image=ButtonImg2, bg="#333333", borderwidth=0, command=lambda: main_function(file_name1, file_name2, 0))
 Button2.grid(row=1, column=0, padx=5, pady=10)
 
-Button3 = Button(root, image=ButtonImg3, bg="#333333", borderwidth=0)
+Button3 = Button(root, image=ButtonImg3, bg="#333333", borderwidth=0, command=lambda: main_function(file_name1, file_name2, 1))
 Button3.grid(row=1, column=1, padx=5, pady=10)
 
-Button4 = Button(root, image=ButtonImg4, bg="#333333", borderwidth=0)
+Button4 = Button(root, image=ButtonImg4, bg="#333333", borderwidth=0, command=lambda: main_function(file_name1, file_name2, 2))
 Button4.grid(row=2, column=0, padx=5, pady=10)
 
-Button5 = Button(root, image=ButtonImg5, bg="#333333", borderwidth=0)
+Button5 = Button(root, image=ButtonImg5, bg="#333333", borderwidth=0, command=lambda: main_function(file_name1, file_name2, 3))
 Button5.grid(row=2, column=1, padx=5, pady=10)
 
-Button6 = Button(root, image=ButtonImg6, bg="#333333", borderwidth=0)
+Button6 = Button(root, image=ButtonImg6, bg="#333333", borderwidth=0, command=lambda: main_function(file_name1, file_name2, 4))
 Button6.grid(row=3, column=0, padx=5, pady=10)
 
 # Button That executes all functions
---
+++ UI/functions.py
@@ -0,0 +1,121 @@
+# import libraries
+import cv2
+from sewar import full_ref
+from DISTS_pytorch.DISTS_pt import *
+from tkinter import *  
+from PIL import ImageTk, Image   
+import os 
+
+
+
+def main_function(file_name1, file_name2, no):
+    global file_name_upscaled
+    global file_save_name
+    # Second window created 
+    top = Toplevel()
+    top.title("second window")
+    top.configure(bg="#333333")
+
+    # read passed images 
+    img = cv2.imread(file_name1)
+    ref_img = cv2.imread(file_name2)
+
+    """
+                                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                                    Interpolation
+                                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    """
+
+    # Linear interpolation
+    if no == 0:
+        img_int = cv2.resize(img,(384,384),fx=0,fy=0, interpolation = cv2.INTER_LINEAR)
+        file_save_name = 'linear_interpolation.png'
+        cv2.imwrite(file_save_name, img_int)
+
+    # Bicubic interpolation
+    elif no == 1:
+        img_int = cv2.resize(img,(384,384),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+        file_save_name = 'bicubic_interpolation.png'
+        cv2.imwrite(file_save_name, img_int) 
+    
+    # Lanczos Interpolation
+    elif no == 2:
+        img_int = cv2.resize(img,(384,384),fx=0,fy=0, interpolation = cv2.INTER_LANCZOS4)
+        file_save_name = 'Lanczos.png'
+        cv2.imwrite(file_save_name, img_int)
+    
+    # Nearest Neighbor Interpolation 
+    elif no == 3:
+        img_int = cv2.resize(img,(384,384),fx=0,fy=0, interpolation = cv2.cv2.INTER_NEAREST)
+        file_save_name = 'Nearest.png'
+        cv2.imwrite(file_save_name, img_int) 
+    
+    # Pixel area Resampling
+    elif no == 4:
+        img_int = cv2.resize(img,(384,384),fx=0,fy=0, interpolation = cv2.INTER_AREA)
+        file_save_name = 'pixel.png'
+        cv2.imwrite(file_save_name, img_int)
+    
+    else:
+        pass
+
+    """
+                                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                                 Image Quality Assessment
+                                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    """
+
+
+   
+
+    """
+                                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                                 Designing TKINTER GUI
+                                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    """
+
+    # display labels 
+    label_HR = Label(top, text="HR Image", bg="#333333", fg="#fff")
+    label_HR.grid(row=0, column=0)
+    label_LR = Label(top, text="LR Image", bg="#333333", fg="#fff")
+    label_LR.grid(row=0, column=1)
+    label_Upscaled = Label(top, text="Upscaled Image", bg="#333333", fg="#fff")
+    label_Upscaled.grid(row=0, column=2)
+
+    # display the image
+
+    # display HR image
+    disp_HR = ImageTk.PhotoImage(Image.open(file_name2))  
+    label_HR_img = Label(top, image = disp_HR, bg="#333333", fg="#fff")
+    label_HR_img.grid(row=1, column=0)
+
+    # display LR image
+    disp_LR = ImageTk.PhotoImage(Image.open(file_name1))  
+    label_LR_img = Label(top, image = disp_LR, bg="#333333", fg="#fff")
+    label_LR_img.grid(row=1, column=1)
+
+    # display upscaled image
+    file_name_upscaled = os.path.abspath(file_save_name)
+    disp = ImageTk.PhotoImage(Image.open(file_name_upscaled))
+    label_1 = Label(top, image=disp, bg="#333333", fg="#fff")
+    label_1.grid(row=1, column=2)
+
+    # calculate the MS-SSIM and DISTS
+    msssim_img=full_ref.msssim(ref_img, img_int, weights=[0.0448, 0.2856, 0.3001, 0.2363, 0.1333], ws=11, K1=0.01, K2=0.03, MAX=None)
+    dists_img = arguments(file_name2, file_name_upscaled)
+
+    
+    # display the calculated error
+    label_msssim = Label(top, text = "MS-SSIM: "+str(msssim_img), bg="#333333", fg="#fff")
+    label_msssim.grid(row=2, column=0)
+
+    label_dists = Label(top, text = "DISTS: "+str(dists_img), bg="#333333", fg="#fff")
+    label_dists.grid(row=2, column=2)
+
+    # end of program
+    mainloop()
+
+
+   
+    
+    
